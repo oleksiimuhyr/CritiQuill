4f66144d62d1c1c17294668354dacd979d4c2477
Oleksii Muhyr <1.2.aznch@gmai.com>
2024-03-25 16:37:35 +0200

    add modifications and create new files to represent list views

--- review/urls.py
+++ review/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from . import views
-
+from .views import GenreListView, MovieListView, ReviewListView, ReviewerListView
 
 app_name = "review"
 
 urlpatterns = [
     path("review/", views.index, name="index"),
+    path("genres/", GenreListView.as_view(), name="genres-list"),
+    path("movies/", MovieListView.as_view(), name="movies-list"),
+    path("reviews/", ReviewListView.as_view(), name="reviews-list"),
+    path("reviewers/", ReviewerListView.as_view(), name="reviewers-list"),
 ]
--- review/views.py
+++ review/views.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.decorators import login_required
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
-from .models import Movie, Review, Reviewer, Genre
+from django.views import generic
+
+from .models import Movie, Review, Genre, Reviewer
 
 
 @login_required
@@ -23,3 +25,31 @@ def index(request):
     }
 
     return render(request, "review/index.html", context=context)
+
+
+class GenreListView(generic.ListView):
+    model = Genre
+    template_name = "review/genres_list.html"
+    context_object_name = "genres_list"
+    paginate_by = 5
+
+
+class MovieListView(generic.ListView):
+    model = Movie
+    template_name = "review/movies_list.html"
+    context_object_name = "movies_list"
+    paginate_by = 5
+
+
+class ReviewListView(generic.ListView):
+    model = Review
+    template_name = "review/reviews_list.html"
+    context_object_name = "reviews_list"
+    paginate_by = 5
+
+
+class ReviewerListView(generic.ListView):
+    model = Reviewer
+    template_name = "review/reviewers_list.html"
+    context_object_name = "reviewer_list"
+    paginate_by = 5
