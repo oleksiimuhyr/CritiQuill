0e62f8fdb7c1892709f7f5082a7648fb977c6821
Oleksii Muhyr <1.2.aznch@gmai.com>
2024-03-28 12:33:57 +0200

    add validation for release_year field

--- review/forms.py
+++ review/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 from django.core.exceptions import ValidationError
+from django.utils import timezone
 
 from review.models import Movie, Genre, Review, Reviewer
 
@@ -15,6 +16,13 @@ class MovieForm(forms.ModelForm):
     class Meta:
         model = Movie
         fields = ['title', 'description', 'genres', 'release_year']
+
+    def clean_release_year(self):
+        release_year = self.cleaned_data.get('release_year')
+        current_year = timezone.now().year
+        if release_year < 1888 or release_year > current_year:
+            raise ValidationError("Release year must be between 1888 and the current year.")
+        return release_year
 
 
 class GenreForm(forms.ModelForm):
