108100f08e3324f5a428ef93631c7f4c852b4967
Oleksii Muhyr <1.2.aznch@gmai.com>
2024-04-01 18:41:45 +0300

    change settings.py and mistake in footer.html

--- MoviePlatform/settings.py
+++ MoviePlatform/settings.py
@@ -140,10 +140,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
+STATIC_URL = "static/"
 
 STATIC_ROOT = "staticfiles/"
-
-STATIC_URL = "/static/"
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
