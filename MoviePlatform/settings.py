f0ebeadd968c33a0f3e709a83ab00c2f3148f977
Oleksii Muhyr <1.2.aznch@gmai.com>
2024-04-01 18:05:55 +0300

    changed settings

--- MoviePlatform/settings.py
+++ MoviePlatform/settings.py
@@ -37,7 +37,7 @@ DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1",
-                 "https://critiquill.onrender.com"]
+                 "critiquill.onrender.com"]
 
 
 # Application definition
